935ba76c731dd6417b057222626baf95f603d46d
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-06 00:21:05 -0500

    plugins and files

---
+++ static/js/user.js
@@ -0,0 +1,71 @@
+var requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+function setLine(i) {
+    context.lineWidth = strokeWidth[i];
+    if (colors[i] != null) {
+        context.strokeStyle = colors[i];
+    } else {
+        context.strokeStyle = $('#cp2').colorpicker('getValue');
+    }
+    if (opacity[i] != null) {
+        context.globalAlpha = opacity[i];
+    } else {
+        context.globalAlpha = parseFloat($("#opacity").val());
+    }
+    context.beginPath();
+    if (clickDrag[i] && i) {
+        context.moveTo(clickX[i - 1], clickY[i - 1]);
+    } else {
+        context.moveTo(clickX[i] - 1, clickY[i]);
+    }
+    context.lineTo(clickX[i], clickY[i]);
+    context.closePath();
+    context.stroke();
+}
+
+
+function redraw() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    context.lineJoin = "round";
+
+    for (var i = 0; i < clickX.length; i++) {
+        setLine(i);
+    }
+}
+
+function clearCanvas(reset) {
+    if (reset) {
+        clickX = new Array();
+        clickY = new Array();
+        clickDrag = new Array();
+    }
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+}
+
+
+function animate() {
+    clearCanvas(false);
+    animateLines();
+}
+
+var lineCount = 1;
+
+function animateLines() {
+    i = lineCount;
+    setLine(i);
+    lineCount += 1;
+
+    if (i > clickX.length) {
+        cancelAnimationFrame(requestID);
+    } else {
+        requestID = requestAnimationFrame(animateLines);
+    }
+}
+
+$(document).ready(function() {
+    animate();
+});
