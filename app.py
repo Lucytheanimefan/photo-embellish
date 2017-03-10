274f5eda118818fc73e46c0ded9d61913d8afcad
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-09 23:15:37 -0500

    Trouble shoot file path

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def code_file(file):
 def create_files():
 	print "CREATE FILES"
 	print "js_values? in createfiles"
-	min_css=minify_text("/static/css/style.css", "css")
+	min_css=minify_text("static/css/style.css", "css")
 	print "----finished minifying min_css-------"
-	min_js = js_values + minify_text("/static/js/user.js", "js")+"animate();"
+	min_js = js_values + minify_text("static/js/user.js", "js")+"animate();"
 	"----finished minifying min_js-------"
 	data = {"css":min_css, "js":min_js}
 	print "should be returning in create_files"
@@ -65,9 +65,11 @@ def write_to_file(content, file_name):
 def minify_text(filepath, file_type):
 	print "in minify_text"
 	url = os.path.realpath('.')
+	print "url: "+url
 	print(SITE_URL+filepath)
-	text = requests.get(filepath).content
-	print text
+	print requests.get(SITE_URL+filepath)
+	text = requests.get(SITE_URL+filepath).content
+	#print text
 	if file_type is "js":
 		minified = minify(text)
 	elif file_type is "css":
